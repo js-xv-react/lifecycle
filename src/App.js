f0faa61f3a37461c070a499070f7b9f82ae94e0a
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2023-02-17 22:03:43 +0100

    componentDidMount shouldComponentUpdate methode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import ChildComponent from "./components/ChildComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "This name will change in 3 sec",
+    };
+  }
+
+  // componentDidMount() {
+  //   console.log("ComponentDidMount");
+  //   setTimeout(() => {
+  //     this.setState({ name: `The name was changed ${Math.random()}` });
+  //   }, 3000);
+
+  //   // usuall purpose
+  //   fetch("https://jsonplaceholder.typicode.com/posts")
+  //     .then((response) => response.json())
+  //     .then((json) => console.log(json));
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <p>{this.state.name}</p>
+        <ChildComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
